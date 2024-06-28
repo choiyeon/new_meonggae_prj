35f661fbc5f452ef25b83902bf7dcbffe92334ce
YouJaeHee <l22991525@naver.com>
2024-06-28 12:14:39 +0900

    마이페이지-프로필사진 수정, 프로필정보 수정, 탈퇴 기능 추가(마이페이지끝)

--- meonggae_prj/src/main/java/com/store/meonggae/my/contorller/PersonalIfoController.java
+++ meonggae_prj/src/main/java/com/store/meonggae/my/contorller/PersonalIfoController.java
@@ -2,8 +2,8 @@ package com.store.meonggae.my.contorller;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -11,26 +11,26 @@ import javax.servlet.http.HttpSession;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.encrypt.Encryptors;
-import org.springframework.security.crypto.encrypt.TextEncryptor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.oreilly.servlet.MultipartRequest;
-import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 import com.store.meonggae.my.domain.PersonalDomain;
 import com.store.meonggae.my.service.PersonalService;
+import com.store.meonggae.my.vo.PersonalInfoVO;
 import com.store.meonggae.user.login.domain.LoginDomain;
 
 @RequestMapping("/My/mypage")
 @Controller
 public class PersonalIfoController {
+	
+	private static String fileDir = "D:/new_meonggae_prj/meonggae_prj/src/main/webapp/profile-img";
 	
 	@Autowired(required = false)
 	private PersonalService ps;
@@ -47,14 +47,11 @@ public class PersonalIfoController {
 			return "/My/mypage/main/myPageMain_frm";
 		}//end if
 		
-		//String memNum = String.valueOf(userSession.getMemNum());
-		
 		return "/My/mypage/personal/passwordChk_frm";
 	}//personal
 	
 	/**
-	 * 마이페이지 : 개인정보_비밀번호검증
-	 * 비밀번호 확인(ajax)
+	 * 마이페이지 : 개인정보_비밀번호검증(ajax)
 	 */
 	@GetMapping("/personal/pwChk.do")
 	@ResponseBody
@@ -84,8 +81,7 @@ public class PersonalIfoController {
 	}//pwChk
 	
 	/**
-	 * 마이페이지 : 개인정보_수정
-	 * @return
+	 * 마이페이지 : 개인정보_개인정보 수정페이지
 	 */
 	@GetMapping("/personal/personalInfo_frm.do")
 	public String personalInfo(HttpServletRequest request, Model model) {
@@ -103,54 +99,18 @@ public class PersonalIfoController {
 		
 		return "/My/mypage/personal/personalInfo_frm";
 	}//personalInfo
-	
+
 	/**
-	 * 마이페이지 : 개인정보_회원탈퇴
-	 */
-	@GetMapping("/personal/quitMembership_frm.do")
-	public String quitMembership() {
-		return "/My/mypage/personal/quitMembership_frm";
-	}//quitMembership
-	
-	/**
-	 * 개인정보 수정 파일 업로드
-	 */
-	//@PostMapping("/personal/upload_profile_process.do")
-	@RequestMapping(value="/personal/upload_profile_process.do", method=RequestMethod.POST)
-	// 1. httpservletRequest 받기
-	public String profileUploadProcess(HttpServletRequest request, Model model) throws IOException {
-		
-		File saveDir = new File("D:/meonggae_store_prj/meonggae_prj/src/main/webapp/profile-img");
-		int tempSize =100*1024*1024;
-		MultipartRequest mr = new MultipartRequest(request, saveDir.getAbsolutePath(),
-				tempSize,"UTF-8");
-		
-		String oriName = mr.getOriginalFileName("upfile");
-		String fsName = mr.getFilesystemName("upfile");
-		
-		File tempFile = new File(saveDir.getAbsolutePath() + "/" + fsName);
-		int maxSize = 10*1024*1024;
-		boolean uploadflag = false;
-		
-		if(tempFile.length() > maxSize) { //업로드 제한
-			tempFile.delete();
-			uploadflag = true;
-		}//end if
-		
-		model.addAttribute("fileName", oriName);
-		model.addAttribute("uploadflag", !uploadflag);
-		
-		return "/My/mypage/personal/personalInfo_frm";
-	}//profileUploadProcess
-	
-	/**
-	 * 마이페이지 : 개인정보_비밀번호수정
+	 * 마이페이지 : 개인정보_비밀번호 수정
 	 */
 	@GetMapping("/personal/passwordModify.do")
 	public String passwordModify() {
 		return "/My/mypage/personal/passwordModify";
 	}//passwordModify
 	
+	/**
+	 * 마이페이지 : 개인정보_비밀번호 수정 process
+	 */
 	@GetMapping("/personal/doPasswordModify.do")
 	public String doPasswordModify(HttpServletRequest request) {
 		
@@ -167,10 +127,172 @@ public class PersonalIfoController {
 		String inputPw = request.getParameter("pw1");
 		
 		int cnt = ps.modifyPw(memNum, inputPw);
-		System.out.println(cnt + "건 업데이트");
 		
-		return "";
+		return "/My/mypage/personal/close";
 	}//passwordModify
 	
+	/**
+	 * 마이페이지 : 닉네임 중복확인
+	 */
+	@GetMapping("/personal/chkNick.do")
+	public String chkNick() {
+		return "/My/mypage/personal/nick_chk";
+	}//passwordModify
+	
+	/**
+	 * 마이페이지 : 닉네임 중복확인 process
+	 */
+	@GetMapping("/personal/doChkNick.do")
+	@ResponseBody
+	public JSONObject doChkNick(@RequestParam("data") String nick) {
+		JSONObject jsonObj = new JSONObject();
+		
+		if(ps.searchNick(nick)) {
+			jsonObj.put("result", "true");
+		}else {
+			jsonObj.put("result", "false");
+		}//end else
+		
+		return jsonObj;
+	}//doChkNick
+	
+	/**
+	 * 창 닫아버리기용...
+	 */
+	@GetMapping("/personal/close.do")
+	public String close() {
+		return "/My/mypage/personal/close";
+	}//close
+	
+	/**
+	 * 개인정보 수정 파일 업로드
+	 */
+	//@RequestMapping(value="/personal/update_personal_info_process.do", method=RequestMethod.POST)
+	@PostMapping("/personal/update_personal_info_process.do")
+	@ResponseBody
+	public JSONObject updatePersonalInfo(HttpServletRequest request) throws Exception {
+		JSONObject jsonObj = new JSONObject();
+		
+		HttpSession session = request.getSession();
+		LoginDomain userSession = (LoginDomain)session.getAttribute("user");
+		
+		
+		if(userSession == null) {
+			jsonObj.put("result", "noSession");
+			return jsonObj;
+		}//end if
+		
+		String memNum = String.valueOf(userSession.getMemNum());
+		String profile = userSession.getimg();
+		
+		// 파일업로드
+		File saveDir = new File(fileDir);
+		int tempSize =100*1024*1024;
+		MultipartRequest mr = new MultipartRequest(request, saveDir.getAbsolutePath(),
+				tempSize,"UTF-8");
+		
+		String fileName = mr.getFilesystemName("upfile"); //업로드 파일 이름(확장자까지)
+		
+		String profileName = "";
+		Boolean fileFlag = false;
+		
+		if(fileName != null) {
+			fileFlag = true ;
+			
+			File oldFile = new File(fileDir + "/" + fileName);
+			int maxSize = 10*1024*1024;
+			
+			if(oldFile.length() > maxSize) { //업로드 제한
+				oldFile.delete();
+				jsonObj.put("result", "tooBig");
+				return jsonObj;
+			}//end if
+			
+			//이름바꾸기
+			String now = new SimpleDateFormat("yyyyMMddHmsS").format(new Date());
+			profileName = memNum + "_" + now + fileName.substring(fileName.lastIndexOf("."), fileName.length());
+			
+			File newFile = new File(fileDir + "/" + profileName); //이름바꿀파일
+			oldFile.renameTo(newFile);
+			
+			if(!profile.equals("default.png")) {
+				File deleteFile = new File(fileDir + "/" + profile);
+				deleteFile.delete();
+			}else {
+			}
+		}//if 파일업로드 끝
+		
+		String nickname = mr.getParameter("nickname");
+		String tel = mr.getParameter("phoneNumber");
+		String zipcode = mr.getParameter("zipcode");
+		String addr0 = mr.getParameter("addr0");
+		String addr1 = mr.getParameter("addr1");
+		
+		PersonalInfoVO piVO = new PersonalInfoVO();
+		piVO.setMemnun(memNum);
+		piVO.setNick(nickname);
+		piVO.setTel(tel);
+		piVO.setZipcode(zipcode);
+		piVO.setAddr0(addr0);
+		piVO.setAddr1(addr1);
+		
+		if(fileFlag) {
+			piVO.setProfile(profileName);
+		}//if
+		
+		int cnt = ps.modifyPersonalInfo(piVO);
+		jsonObj.put("result", "success");
+		
+		//세션에 설정해줘야함...
+		userSession.setNick(nickname);
+		if(fileFlag) {
+			userSession.setimg(profileName);
+		}//if
+		
+		session.setAttribute("user", userSession);
+		
+		return jsonObj;
+	}//profileUploadProcess
+	
+	/**
+	 * 회원탈퇴
+	 */
+	@GetMapping("/personal/quitMember.do")
+	public String quitMember(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		LoginDomain userSession = (LoginDomain)session.getAttribute("user");
+		
+		if(userSession == null) {
+			return "/My/mypage/main/myPageMain_frm";
+		}//end if
+		
+		return "/My/mypage/personal/quitMember";
+	}//quitMember
+	
+	/**
+	 * 회원탈퇴 Process
+	 */
+	@GetMapping("/personal/quitProcess.do")
+	@ResponseBody
+	public JSONObject quitProcess(HttpServletRequest request) {
+		JSONObject jsonObj = new JSONObject();
+		
+		HttpSession session = request.getSession();
+		LoginDomain userSession = (LoginDomain)session.getAttribute("user");
+		
+		if(userSession == null) {
+			jsonObj.put("result", "noSession");
+			return jsonObj;
+		}//end if
+		
+		String memNum = String.valueOf(userSession.getMemNum());
+		
+		int cnt = ps.modfiyMemberStatus(memNum);
+		if(cnt == 1) {
+			jsonObj.put("result", "success");
+		}//if
+		
+		return jsonObj;
+	}//quitProcess
 	
 }//class
---
+++ meonggae_prj/src/main/webapp/WEB-INF/views/My/mypage/personal/close.jsp
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Close Window</title>
+    <script type="text/javascript">
+        window.onload = function() {
+            window.close();
+        };
+    </script>
+</head>
+<body>
+    <p>Closing the window...</p>
+</body>
+</html>
