ef6434146a930b43707e8258add79cfb5f3a7349
yuni4019 <yuni4019@gmail.com>
2024-07-05 19:56:30 +0900

    문의,신고 dao domain service controller vo mapper 첫 커밋

---
+++ meonggae_prj/src/main/java/com/store/meonggae/mng/report/dao/MngReportDAO.java
@@ -0,0 +1,68 @@
+package com.store.meonggae.mng.report.dao;
+
+import java.util.List;
+
+
+import org.apache.ibatis.exceptions.PersistenceException;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.store.meonggae.mng.dao.MyBatisDAO;
+import com.store.meonggae.mng.report.domain.MngReportDomain;
+import com.store.meonggae.mng.search.vo.SearchVO;
+
+
+@Component
+public class MngReportDAO {
+
+	@Autowired(required = false)
+	private MyBatisDAO mbDAO; 
+	
+	public List<MngReportDomain> selectReport( SearchVO sVO)throws PersistenceException {
+		
+		List<MngReportDomain> list=null;
+		SqlSession ss=mbDAO.getMyBatisHandler(false);
+		System.out.println("-----------"+ list);
+		list=ss.selectList("kr.co.sist.report.selectReportList", sVO);
+		mbDAO.closeHandler(ss);
+		return list;
+	}
+	
+	 public int selectTotalCount(SearchVO sVO) throws PersistenceException {
+		 
+	        int totalCount = 0;
+	        SqlSession ss = mbDAO.getMyBatisHandler(false);
+	        totalCount = ss.selectOne("kr.co.sist.report.selectTotalCount", sVO);
+	        mbDAO.closeHandler(ss);
+	        return totalCount;
+	    }
+	 
+	 public MngReportDomain selectDetailReport(String rep_num) throws PersistenceException {
+		 
+		    MngReportDomain report = null;
+		    SqlSession ss = mbDAO.getMyBatisHandler(false);
+		    report = ss.selectOne("kr.co.sist.report.selectDetailReport", rep_num);
+		    mbDAO.closeHandler(ss);
+		    return report;
+		}
+
+	 
+	 public void updateReport(MngReportDomain rd) throws PersistenceException {
+	        SqlSession ss = mbDAO.getMyBatisHandler(true);
+	        try {
+	        	System.out.println(rd);
+	            ss.update("kr.co.sist.report.updateReport", rd);
+	        
+	        } catch (PersistenceException pe) {
+	         System.out.println(pe);
+	           
+	        } finally {
+	            mbDAO.closeHandler(ss);
+	        }
+	    }
+	 
+	   
+	
+}
+  
---
+++ meonggae_prj/src/main/java/com/store/meonggae/mng/report/domain/MngReportDomain.java
@@ -0,0 +1,23 @@
+package com.store.meonggae.mng.report.domain;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class MngReportDomain {
+
+	private int mem_num_rep;
+	private String rep_num, manager_id, goods_num, category, title,
+	contents, process_status_code, process_contents;
+	private Date input_date, process_date;
+	
+}
