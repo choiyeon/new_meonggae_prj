8277a6c3ff50924058fc9f71ab7cb1400e667ba0
yuni4019 <yuni4019@gmail.com>
2024-07-05 21:50:16 +0900

    문의 컨트롤러 수정

--- meonggae_prj/src/main/java/com/store/meonggae/mng/inquiry/controller/MngInquiryController.java
+++ meonggae_prj/src/main/java/com/store/meonggae/mng/inquiry/controller/MngInquiryController.java
@@ -3,6 +3,7 @@ package com.store.meonggae.mng.inquiry.controller;
 import java.util.List;
 
 
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -11,13 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.store.meonggae.mng.inquiry.domain.MngInquiryDomain;
 import com.store.meonggae.mng.inquiry.service.MngInquiryService;
 import com.store.meonggae.mng.search.vo.SearchVO;
-
 
 
 @Controller
